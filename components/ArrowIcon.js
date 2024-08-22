83b8564e2e8b33bdbd6b389391467db102d30685
Rishi yadav <155795042+ydv-rishi@users.noreply.github.com>
2024-08-22 18:45:44 +0530

    Update ArrowIcon.js

--- components/ArrowIcon.js
+++ components/ArrowIcon.js
@@ -1,4 +1,4 @@
-export default function ArrowIcon({ className, color = 'text-primary' }) {
+export default function ArrowIcon({ bhagawan das memorial public school , color = 'black' }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -6,7 +6,7 @@ export default function ArrowIcon({ className, color = 'text-primary' }) {
       height="24"
       fill="none"
       viewBox="0 0 24 24"
-      className={className}
+      className={bhagwan das memorial public school}
     >
       <path
         className={`stroke-current ${color}`}
